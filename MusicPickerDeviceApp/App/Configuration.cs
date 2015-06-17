38613875fba3758ed3b9c90eb5834b9d276641aa
Pete <piierre.d@live.fr>
2015-06-17 18:17:29 +0200

    Configuration storage

---
+++ MusicPickerDeviceApp/App/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace MusicPickerDeviceApp.App
+{
+    private class ConfigurationModel
+    {
+        public int DeviceId { get; set; }
+        public string Bearer { get; set; }
+        public string[] Paths { get; set; }
+
+    }
+
+    public class Configuration
+    {
+        public ConfigurationModel Model;
+        public void Load()
+        {
+            this.Model = JsonConvert.DeserializeObject<ConfigurationModel>(Convert.ToString(File.ReadAllBytes("musicpicker.json")));
+        }
+
+        public void Save()
+        {
+            File.WriteAllText("musicpicker.json", JsonConvert.SerializeObject(this.Model));
+        }
+    }
+}
